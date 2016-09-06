95d87d942914d664af56ae861824e72e9a44c3ee
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-06 14:37:50 +0200

    dscreeps-grunt

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = grunt => {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: { banner: "'use strict'; // v<%= pkg.version %>\n\n" },
+      dist: {
+        src: ['src/module.js', 'src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      live: {
+        src: ['src/module.js', 'src/**/*.js'],
+        dest: '../<%= pkg.name %>.js'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat']);
+};
